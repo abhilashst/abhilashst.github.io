da30dde2e149206e234fbc31cae16217c8cf3350
Abhilash S T <abhilashst1@gmail.com>
2017-11-23 14:22:15 +0530

    Update sliderstyle.js

--- js/sliderstyle.js
+++ js/sliderstyle.js
@@ -1,2 +1,1 @@
 if (!document.getElementById) document.write('<link rel="stylesheet" type="text/css" href="https://abhilashst.github.io/css/simple-slider.css">');
-if (!document.getElementById) document.write('<link rel="stylesheet" type="text/css" href="https://abhilashst.github.io/css/simple-slider-volume.css">');
