be8cdd393a1dd47b33793744b7437335eb39383a
Abhilash S T <abhilashst1@gmail.com>
2017-11-23 11:52:35 +0530

    Create sliderstyle.js

---
+++ js/sliderstyle.js
@@ -0,0 +1,2 @@
+if (!document.getElementById) document.write('<link rel="stylesheet" type="text/css" href="https://abhilashst.github.io/css/simple-slider.css">');
+if (!document.getElementById) document.write('<link rel="stylesheet" type="text/css" href="https://abhilashst.github.io/css/simple-slider-volume.css">');
