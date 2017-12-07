ff153cef15587aa276cb3a0334fdcf7ec687c6da
Abhilash S T <abhilashst1@gmail.com>
2017-12-07 15:03:28 +0530

    Update rd.js

--- js/rd.js
+++ js/rd.js
@@ -17,12 +17,13 @@ var final = rupee + accumulateMonthlyAmount.toFixed(2).toString().replace(/,/g, 
 var finalInterestGain = accumulateMonthlyAmount - (monthlyInstallment * numberOfMonths);
 //var accumulateMonthlyAmount2 = parseInt(monthlyInstallment) * ((Math.pow(rateOfInterestFixed / 400 + 1, ci) - 1) / (1-(Math.pow(rateOfInterestFixed / 400 + 1,(-1/3)))));
 var accumulateMonthlyAmount2 =  monthlyInstallment  *(Math.pow((1+ irate2/100),(year)*ci)-1)/(1-Math.pow((1+irate2/100),-ci/12));
-
 var depositedAmount = monthlyInstallment * numberOfMonths;
+var benefit= accumulateMonthlyAmount2 - depositedAmount;
+
 $("#tbl_de").html(rupee + depositedAmount.toFixed(0).toString().replace(/,/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ","));
 $("#tbl_full").html(final);
 $("#tbl_int").html(rupee + finalInterestGain.toFixed(2).toString().replace(/,/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ","));
-$("#tbl_mf").html(rupee + accumulateMonthlyAmount2.toFixed(2).toString().replace(/,/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+$("#tbl_mf").html(rupee + benefit.toFixed(2).toString().replace(/,/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ","));
 
 		var chart = new Highcharts.Chart({
 				 
